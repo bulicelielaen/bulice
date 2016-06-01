af174b3dceb7d356852ac40c38015bcbd614535b
Buliceli <lidongdong@buliceli.local>
2016-06-01 13:56:05 +0800

    新增加了类

---
+++ GitDemo/GitDemo/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  GitDemo
+//
+//  Created by 李洞洞 on 1/6/16.
+//  Copyright © 2016年 Minte. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ GitDemo/GitDemo/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  GitDemo
+//
+//  Created by 李洞洞 on 1/6/16.
+//  Copyright © 2016年 Minte. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
