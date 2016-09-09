90f647263bdd99bbd0d3655f65b38f3ec00b5d16
buliceli <lidongdong@buliceli.local>
2016-09-09 09:42:32 +0800

    新建工程

---
+++ 666/666/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  666
+//
+//  Created by 李洞洞 on 9/9/16.
+//  Copyright © 2016年 Minte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 666/666/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  666
+//
+//  Created by 李洞洞 on 9/9/16.
+//  Copyright © 2016年 Minte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
